62d9d5a378ab9e4149a10b6c501bf0b2a5f41964
SD Flatres <sdf@marlboroughcollege.org>
2024-11-12 11:04:40 +0000

    oled

---
+++ 3_OLED/1_oled.py
@@ -0,0 +1,61 @@
+# import libraries
+import board, busio, displayio, digitalio, time, terminalio
+
+from i2cdisplaybus import I2CDisplayBus
+from adafruit_display_text import label
+import adafruit_displayio_ssd1306
+
+# Setup
+displayio.release_displays()
+i2c = busio.I2C(board.GP17, board.GP16)  # uses board.SCL and board.SDA
+display_bus = I2CDisplayBus(i2c, device_address=0x3c)
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=32)
+display.root_group = displayio.Group()
+
+switch1 = digitalio.DigitalInOut(board.GP14)
+switch1.direction = digitalio.Direction.INPUT
+switch1.pull = digitalio.Pull.UP
+
+# Draw a splash screen
+text = "Hello World!"
+text_area = label.Label(terminalio.FONT, text=text, x=30, y=15)
+display.root_group.append(text_area)
+time.sleep(1)
+
+seconds = 0.0
+started = False
+
+while True:
+    
+    if switch1.value == False:
+        started = True
+        
+    if started == True:
+        
+        #clear the display     
+        display.root_group = displayio.Group()
+    
+        #update time
+        text_area = label.Label(terminalio.FONT, text=str(seconds), x=60, y=16)
+        display.root_group.append(text_area)
+        
+        seconds = seconds + 0.1
+        
+        time.sleep(0.1)
+    
+
+'''CHALLENGES
+
+1. Change the splash screen so that the user is given instructions on how to use the timer 
+
+2. Modify the code so that pressing the button again pauses the timer
+
+3. Add a second button that can be used to reset the timer
+
+4. Add an RGB LED that glows red when the timer is stopped and green when it is running
+
+5. Modify the code so that the timer counts down from 10 seconds and displays tenths of a second.
+
+'''
+
+
