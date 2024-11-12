067d8ab9c667ccda7b94d37bbf3ed941814422e1
SD Flatres <sdf@marlboroughcollege.org>
2024-11-12 14:10:45 +0000

    ..

--- 3_OLED/1_oled.py
+++ 3_OLED/1_oled.py
@@ -9,7 +9,7 @@ import adafruit_displayio_ssd1306
 displayio.release_displays()
 i2c = busio.I2C(board.GP17, board.GP16)  # uses board.SCL and board.SDA
 display_bus = I2CDisplayBus(i2c, device_address=0x3c)
-display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=32)
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 display.root_group = displayio.Group()
 
 switch1 = digitalio.DigitalInOut(board.GP14)
@@ -18,7 +18,7 @@ switch1.pull = digitalio.Pull.UP
 
 # Draw a splash screen
 text = "Hello World!"
-text_area = label.Label(terminalio.FONT, text=text, x=30, y=15)
+text_area = label.Label(terminalio.FONT, text=text, x=30, y=32)
 display.root_group.append(text_area)
 time.sleep(1)
 
@@ -36,7 +36,7 @@ while True:
         display.root_group = displayio.Group()
     
         #update time
-        text_area = label.Label(terminalio.FONT, text=str(seconds), x=60, y=16)
+        text_area = label.Label(terminalio.FONT, text=str(seconds), x=60, y=32)
         display.root_group.append(text_area)
         
         seconds = seconds + 1
