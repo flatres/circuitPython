dcb1a44365cf5abfaa4fbe170c0064abab053d2e
SD Flatres <sdf@marlboroughcollege.org>
2024-11-14 13:39:44 +0000

    libs

--- update.sh
+++ update.sh
@@ -10,7 +10,14 @@ cd libs
 cd adafruit
 cd lib
 cp adafruit_rgbled.mpy /media/usb/lib/
+
+#oled
 cp adafruit_ssd1306.mpy /media/usb/lib/
 cp adafruit_displayio_ssd1306.mpy /media/usb/lib/
 cp adafruit_framebuf.mpy /media/usb/lib/
 cp -r adafruit_display_text /media/usb/lib/
+
+#env sensor AHT20
+cp -r adafruit_bus_device /media/usb/lib/
+cp adafruit_ahtx0.mpy /media/usb/lib/
+
