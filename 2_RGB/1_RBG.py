43ef5a5f45b0eb68f95779d005f0e2c787a4a831
SD Flatres <sdf@marlboroughcollege.org>
2024-11-14 14:01:35 +0000

    ..

--- 2_RGB/1_RBG.py
+++ 2_RGB/1_RBG.py
@@ -12,15 +12,17 @@ BLUE = board.GP0
 # Create the RGB LED object
 led = adafruit_rgbled.RGBLED(RED, GREEN, BLUE)
 
-switch = digitalio.DigitalInOut(board.GP17)
-switch.direction = digitalio.Direction.INPUT
-switch.pull = digitalio.Pull.UP
+switch1 = digitalio.DigitalInOut(board.GP17)
+switch1.direction = digitalio.Direction.INPUT
+switch1.pull = digitalio.Pull.UP
 
 while True:
   led.color = (255, 0, 0)
-  if switch.value == False:
+  if switch1.value == False:
       led.color = (0, 255, 0)
       #time.sleep(0.2)
+      
+#CHALLENGE
 
 # task 1: add a 0.2 second delay after the color
 # changes to green. What happens?
