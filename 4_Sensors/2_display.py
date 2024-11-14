07bb89849def76ec58522b93ba113d2fe3e62269
SD Flatres <sdf@marlboroughcollege.org>
2024-11-14 13:57:58 +0000

    ..

--- 4_Sensors/2_display.py
+++ 4_Sensors/2_display.py
@@ -8,15 +8,14 @@ import adafruit_displayio_ssd1306
 import adafruit_ahtx0
 
 #Setup
+i2c = busio.I2C(board.GP17, board.GP16)  # uses board.SCL and board.SDA
 
 displayio.release_displays()
-i2c = busio.I2C(board.GP17, board.GP16)  # uses board.SCL and board.SDA
 display_bus = I2CDisplayBus(i2c, device_address=0x3c)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 display.root_group = displayio.Group()
 
-i2c_2 = busio.I2C(board.GP13, board.GP12) #SCL, SDA
-sensor = adafruit_ahtx0.AHTx0(i2c_2)
+sensor = adafruit_ahtx0.AHTx0(i2c)
 
 while True:
     temperature = sensor.temperature
@@ -30,11 +29,11 @@ while True:
     
     #update data
     text = "Temp: %0.1f C" % temperature
-    text_area = label.Label(terminalio.FONT, text=text, x=60, y=32)
+    text_area = label.Label(terminalio.FONT, text=text, x=5, y=25)
     display.root_group.append(text_area)
     
     text = "Humidity: %0.1f %%" % humidity
-    text_area = label.Label(terminalio.FONT, text=text, x=60, y=42)
+    text_area = label.Label(terminalio.FONT, text=text, x=5, y=42)
     display.root_group.append(text_area)
     
     time.sleep(2)
